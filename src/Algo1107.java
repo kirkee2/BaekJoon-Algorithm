b7044171a8b5cf7ae95270d5e2cf6f211e24a75a
Joon <kirkee2@naver.com>
2017-04-14 00:18:42 +0900

    1107 solved

---
+++ src/Algo1107.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.math.*;
+
+public class Algo1107 {
+
+	static boolean broken[];
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		broken = new boolean[10];
+
+		int target = sc.nextInt();
+		int move = Math.abs(target - 100);
+		int min = Integer.MAX_VALUE;
+		
+		int n = sc.nextInt();
+		
+		for(int i = 0 ; i < n ; i++){
+			broken[sc.nextInt()] = true;
+		}
+		for(int i = 0 ; i<=1000000 ; i++){
+			int c = i;
+			if(possible(i) != 0){
+				if(min > possible(i) + Math.abs(i - target)){
+					min = possible(i) + Math.abs(i - target);
+				}
+			}
+		}
+		
+		if(min>move){
+			System.out.println(move);
+		}else{
+			System.out.println(min);
+		}
+		
+		sc.close();
+	}
+	
+	static int possible(int i){
+		if(i == 0){
+			if(broken[0]){
+				return 0;
+			}else{
+				return 1;
+			}
+		}
+		int len = 0;
+		
+		while(i>0){
+			if(broken[i%10]){
+				return 0;
+			}
+				
+			i /= 10;
+			len++;
+		}
+		return len;
+	}
+
+}
